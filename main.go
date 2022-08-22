755a91c6a2b2ceacbfb4fe5df64ba49b4871ab17
shpendbeqiraj2 <shpendbeqiraj2@gmail.com>
2022-08-23 01:37:16 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"tetris/screen"
+	"tetris/tetris"
+	"time"
+
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	fmt.Println("Tetris")
+	const animationSpeed = 50 * time.Millisecond
+
+	rand.Seed(time.Now().Unix())
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	eventQueue := make(chan termbox.Event)
+	go func ()  {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
+
+	ticker := time.NewTimer(time.Duration(animationSpeed))
+
+	game := tetris.New()
+	scr := screen.New()
+
+	// scr.RenderAsci(game.GetBoard())
+
+	for {
+		select {
+		case ev := <- eventQueue:
+			if ev.Type == termbox.EventKey {
+				switch  {
+				case ev.Key == termbox.KeyArrowUp:
+					game.Rotate()
+				case ev.Key == termbox.KeyArrowDown:
+					game.SpeedUp()
+				case ev.Key == termbox.KeyArrowLeft:
+					game.MoveLeft()
+				case ev.Key == termbox.KeyArrowRight:
+					game.MoveRight()
+				case ev.Key == termbox.KeySpace:
+					game.Fall()
+				case ev.Ch == 'q':
+					//quit
+					return
+				case ev.Ch == 's':
+					//start
+					game.Start()
+				}
+			}
+		case <- ticker.C:
+			scr.Render(game.GetBoard())
+			ticker.Reset(animationSpeed)
+		case <- game.FallSpeed.C:
+			game.GameLoop()
+		}
+	}
+}
